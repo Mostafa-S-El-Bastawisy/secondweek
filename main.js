0849a8aa712293625954f6c0447c2e0d41ad38c3
Mostafa S. El-bastawisy <mostafa.sayed112@gmail.com>
2021-07-25 14:08:05 +0200

    Add my file

---
+++ main.js
@@ -0,0 +1,27 @@
+// Data type
+
+console.log("Mostafa El-Bastawisy");
+console.log(typeof("Mostafa El-Bastawisy"));
+console.log(typeof(5000));
+console.log(typeof(5,11));
+console.log(typeof[10, 15, 17]);
+console.log(typeof["Ahmed", "Ali", "Mostafa"]);
+console.log(typeof{Name:"Osama", age: 17, country:"EG"});
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
+
+// Variables 
+
+var user = "Mostafa";
+var age  = 37;
+
+console.log(user);
+console.log(age);
+
+hello.innerHTML = "Option";
+
+
+// Identifiers Name
+
